704b910cd49e3ab4fe8f525c90f2f8f08b147e6e
Mir Muhammad Abidul Haq (Ahnaf) <76505613+Ahnaf-nub@users.noreply.github.com>
2024-07-31 19:36:22 +0600

    Update code.py

--- code.py
+++ code.py
@@ -4,39 +4,16 @@ import digitalio
 import audiomp3
 import time
 import busio
-import time
-import wifi
-import adafruit_requests as requests
-from adafruit_ntp import NTP
+import pwmio
+from adafruit_motor import servo
+import adafruit_hcsr04
+meeting = False
 
-# Wi-Fi credentials
-SSID = "Mahir"
-PASSWORD = "Ahnaf767"
+sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.GP4, echo_pin=board.GP3)
 
-# Connect to Wi-Fi
-print("Connecting to Wi-Fi...")
-wifi.radio.connect(SSID, PASSWORD)
-print("Connected to Wi-Fi!")
-
-# Create a socket pool and requests session
-pool = wifi.radio
-requests = requests.Session(pool)
-
-# Connect to the NTP server
-print("Connecting to NTP server...")
-ntp = NTP(pool, server="pool.ntp.org", tz_offset=0)
-
-# Fetch and print the current time
-current_time = ntp.datetime
-print("Current time:", current_time)
-
-# Convert to local time if needed (example for UTC+2)
-local_time = time.localtime(time.mktime(current_time) + 6 * 3600)
-print("Local time (UTC+6):", local_time)
-
-
-
-audio_out = audiobusio.I2SOut(board.GP0, board.GP1, board.GP2)
+servo_a_pin = pwmio.PWMOut(board.GP5, frequency=50)
+servo_a = servo.Servo(servo_a_pin, min_pulse=1000, max_pulse=2000)
+audio_out = audiobusio.I2SOut(board.GP1, board.GP0, board.GP2)
 
 # Function to play MP3 audio from a file
 def play_audio(file_path):
@@ -46,9 +23,14 @@ def play_audio(file_path):
         while audio_out.playing:
             time.sleep(0.1)
 
-# Main loop to play the louder MP3 file
 while True:
-    play_audio("demo.mp3")
-    time.sleep(1)
-
-
+    dist = sonar.distance/100
+    if (dist <= 15):
+        meeting = True
+    else:
+        meeting = False
+    if meeting == True:
+        play_audio("demo.mp3")
+        servo_a.angle = 90
+    else:
+        servo_a.angle = 0
